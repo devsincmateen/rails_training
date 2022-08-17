7896fb823614e49e4d2f48596101d0a97b7ea509
Mateen Cheema <mateen.haider@devsinc.com>
2022-08-17 18:22:56 +0500

    Day 5 practice

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,7 +5,7 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
-  
+
   def create
     @article = Article.new(article_params)
     if @article.save
--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_220_811_044_304) do
+ActiveRecord::Schema.define(version: 20_220_817_090_221) do
   create_table 'articles', force: :cascade do |t|
     t.string 'title'
     t.text 'text'
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
+  end
+
+  create_table 'comments', force: :cascade do |t|
+    t.string 'commenter'
+    t.text 'body'
+    t.integer 'article_id'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['article_id'], name: 'index_comments_on_article_id'
   end
 end
